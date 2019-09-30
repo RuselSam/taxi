3bb1627a2dc179771cb94b7c0955541fd795b2db
ruslan <ruselsam89@gmail.com>
2019-09-30 23:44:52 +0300

    Added package ru.itpark

--- src/main/java/ru/itpark/TaxiService.java
+++ src/main/java/ru/itpark/TaxiService.java
@@ -1,3 +1,5 @@
+package ru.itpark;
+
 public class TaxiService {
 
 
--- src/test/java/ru/itpark/TaxiServiceTest.java
+++ src/test/java/ru/itpark/TaxiServiceTest.java
@@ -1,4 +1,7 @@
+package ru.itpark;
+
 import org.junit.jupiter.api.Test;
+import ru.itpark.TaxiService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
